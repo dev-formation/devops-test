6f27a329101f743c2a1ce322ca34c6478f75dc33
Jérémy Bojko macMini <jeremy.bojko@protonmail.com>
2022-03-01 18:54:07 +0100

    maj yaml

---
+++ server.js
@@ -0,0 +1,23 @@
+
+/* eslint-env es6 */
+/* eslint-disable no-console */
+const express = require('express');
+const path = require('path');
+const app = express();
+app.use(express.static(__dirname + '/dist/devops-test'));
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname + '/dist/devops-test/index.html'));
+});
+app.listen(process.env.PORT || 8080);
+
+
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('./db/data.json')
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+server.use(router)
+server.listen(3000, () => {
+  console.log('JSON Server is running')
+})
