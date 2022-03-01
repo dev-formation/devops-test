f933e6734f35af20ebcb3979ff6248041b9a62ba
Jérémy Bojko <jeremy.bojko@protonmail.com>
2022-03-01 14:23:45 +0100

    add app-nam server

--- server.js
+++ server.js
@@ -3,9 +3,9 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/<app-name>'));
+app.use(express.static(__dirname + '/dist/devops-test'));
 app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname +
-        '/dist/<app-name>/index.html'));
+        '/dist/devops-test/index.html'));
 });
 app.listen(process.env.PORT || 8080);
